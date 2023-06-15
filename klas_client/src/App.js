c46118eaf91610afb9fc622bbc6c607798fad501
icecoff22 <wjdtmd987@gmail.com>
2023-06-15 14:49:15 +0900

    refactoring: refactor structure

---
+++ klas_client/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Router from "./router.js";
+
+function App() {
+  return (
+    <div className="font-[GmarketSansMedium]">
+      <Router />
+    </div>
+  );
+}
+
+export default App;
+
