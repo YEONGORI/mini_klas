94b0635a9bc0f4faadb7ba4c0c7e101603410281
Icecoff22 <114729161+Icecoff22@users.noreply.github.com>
2023-06-15 10:48:50 +0900

    Update App.js

--- klas_client/src/App.js
+++ klas_client/src/App.js
@@ -1,12 +1,1 @@
-import React from "react";
-import Router from "./router.js";
 
-function App() {
-  return (
-    <div className="font-[GmarketSansMedium]">
-      <Router />
-    </div>
-  );
-}
-
-export default App;
